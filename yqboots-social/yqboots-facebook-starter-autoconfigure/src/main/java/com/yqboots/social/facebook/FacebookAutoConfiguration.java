e9d316dfac7322032d0216cb886be9e256dd3d0f
eric.zhan <eric.zhan@163.com>
2019-05-16 23:52:20 +0800

    facebook

---
+++ yqboots-social/yqboots-facebook-starter-autoconfigure/src/main/java/com/yqboots/social/facebook/FacebookAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.yqboots.social.facebook;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableOAuth2Sso
+public class FacebookAutoConfiguration {
+}
