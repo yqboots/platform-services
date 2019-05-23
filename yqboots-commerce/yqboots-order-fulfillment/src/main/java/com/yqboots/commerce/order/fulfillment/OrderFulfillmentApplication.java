f4a63b4e85192d982992ba21c22a8123dfe415ee
eric.zhan <eric.zhan@163.com>
2019-05-24 00:03:20 +0800

    order fulfillment

---
+++ yqboots-commerce/yqboots-order-fulfillment/src/main/java/com/yqboots/commerce/order/fulfillment/OrderFulfillmentApplication.java
@@ -0,0 +1,11 @@
+package com.yqboots.commerce.order.fulfillment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFulfillmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFulfillmentApplication.class, args);
+    }
+}
