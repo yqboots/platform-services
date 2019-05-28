2d1b8c71afa9eef593e55414d7e77a148929196f
eric.zhan <eric.zhan@163.com>
2019-05-28 22:24:34 +0800

    wechat initiate payment

--- yqboots-social/yqboots-wechat-starter-autoconfigure/src/test/java/com/yqboots/social/wechat/client/builder/InitiatePaymentRequestBuilderTest.java
+++ yqboots-social/yqboots-wechat-starter-autoconfigure/src/test/java/com/yqboots/social/wechat/client/builder/InitiatePaymentRequestBuilderTest.java
@@ -1,23 +1,31 @@
 package com.yqboots.social.wechat.client.builder;
 
 import com.yqboots.social.wechat.WeChatApplication;
-import org.junit.Before;
+import com.yqboots.social.wechat.api.pay.data.InitiatePaymentRequest;
+import com.yqboots.social.wechat.client.builder.support.RequestBuilderParameters;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {WeChatApplication.class})
 public class InitiatePaymentRequestBuilderTest {
+    private static final Logger LOG = LoggerFactory.getLogger(InitiatePaymentRequestBuilderTest.class);
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Autowired
+    private InitiatePaymentRequestBuilder builder;
 
     @Test
     public void build() {
+        InitiatePaymentRequest request = builder.build(new RequestBuilderParameters()
+                .setPrepayId("prepayID001"));
+
+        LOG.info(ToStringBuilder.reflectionToString(request, ToStringStyle.JSON_STYLE));
     }
 }
